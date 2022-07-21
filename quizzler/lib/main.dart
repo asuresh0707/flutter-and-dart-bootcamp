f8525d3b8f74dcb04849c4c7e740dc815c753948
asuresh <suresh.alaparti@gmail.com>
2022-07-21 14:10:35 +0530

    new app quizzler updated

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
+import 'package:quizzler/questionbrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
+QuestionBrain quizBrain = QuestionBrain();
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
@@ -18,6 +22,47 @@ class Quizzler extends StatefulWidget {
 }
 
 class _QuizzlerState extends State<Quizzler> {
+  List<Icon> scoreKeeper = [];
+  // List<String> questions = [
+  //   'hyderbad is the capital city of telangana',
+  //   'tiger is the national animal of india',
+  //   'donald trump is the president of america'
+  // ];
+  //
+  // List<bool> answers = [
+  //   true,
+  //   true,
+  //   false
+  // ];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        Alert(
+                context: context,
+                title: 'Finished',
+                desc: 'You have reached the end of Quiz')
+            .show();
+        quizBrain.reset();
+        scoreKeeper = [];
+      } else {
+        if (correctAnswer == userPickedAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -30,12 +75,9 @@ class _QuizzlerState extends State<Quizzler> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'Hyderbad is Capital City of Telegana',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white
-                ),
+                style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
             ),
           ),
@@ -44,8 +86,11 @@ class _QuizzlerState extends State<Quizzler> {
           child: Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextButton(
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
-              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green)),
+              onPressed: () {
+                checkAnswer(true);
+              },
               child: Text(
                 'True',
                 style: TextStyle(fontSize: 20.0, color: Colors.white),
@@ -57,8 +102,11 @@ class _QuizzlerState extends State<Quizzler> {
           child: Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextButton(
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
-              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red)),
+              onPressed: () {
+                checkAnswer(false);
+              },
               child: Text(
                 'False',
                 style: TextStyle(fontSize: 20.0, color: Colors.white),
@@ -66,6 +114,7 @@ class _QuizzlerState extends State<Quizzler> {
             ),
           ),
         ),
+        Row(children: scoreKeeper),
       ],
     );
   }
